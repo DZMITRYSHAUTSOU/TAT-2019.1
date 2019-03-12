afdc7697d8898a25a022d96393c2c367b0f0881e
DZMITRYSHAUTSOU <d.shautsov2@gmail.com>
2019-03-12 19:42:55 +0300

    Add git ignore, dev-1 v2

---
+++ DEV-1/DEV-1/Sequence.cs
@@ -0,0 +1,52 @@
+﻿using System;
+namespace DEV_1
+{
+    class Sequence
+    {
+        public Sequence() { }
+        public void SequenceSearch(string word)
+        {
+            bool flag = false;//Repeating symbol indicator
+            string seq;//Keeps non-repeating symbols 
+            seq = string.Empty;
+            seq += word[0];
+            for (int i = 1; i < word.Length; i++)
+            {
+                if (word[i] != word[i - 1])
+                {
+                    seq += word[i];
+                }
+                else
+                {
+                    flag = true;
+                }
+                if (flag || (i == word.Length - 1))
+                {
+                    SequenceOutput(seq);//output of all combinations found in 'seq' 
+                    seq = string.Empty;
+                    seq += word[i];
+                    flag = false;
+                }
+            }
+        }
+        public void SequenceOutput(string seq)//Combination outputter
+        {
+            string OutputBuffer = string.Empty;//Buffer for subsequences
+            while (seq.Length >= 2)
+            {
+
+                for (int k = 0; k < seq.Length; k++)
+                {
+                    OutputBuffer += seq[k];
+                    if (OutputBuffer.Length >= 2)
+                    {
+                        Console.WriteLine(OutputBuffer);
+                    }
+
+                }
+                OutputBuffer = string.Empty;
+                seq = seq.TrimStart(seq[0]);
+            }
+        }
+    }
+}
